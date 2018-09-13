b6433b5ac527a0b75c27e6637e27ca181fd6f2ee
Richard Parry <richard.parry@esure.com>
2018-09-13 10:26:13 +0100

    initial version with just simple liquibase scripts with no java classes

---
+++ src/main/java/my/examples/liquibase/ExampleLiquibasePostgresApplication.java
@@ -0,0 +1,12 @@
+package my.examples.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleLiquibasePostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleLiquibasePostgresApplication.class, args);
+	}
+}
